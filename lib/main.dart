cef1dafcebf203f4dc5e9b8526f2926890faee78
Shintaro Miyazaki <tudopassa427911@gmail.com>
2022-10-24 17:15:36 +0900

    changed theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "初めてのapp",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: HomeScreen(),
     );
   }
